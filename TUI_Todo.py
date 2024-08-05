d553f7654ab09a843759304ff7358cd87700085e
Amarikwa <amarikwa188@gmail.com>
2024-08-05 22:37:32 +0100

    implemented save and load to a json file

--- TUI_Todo.py
+++ TUI_Todo.py
@@ -3,61 +3,58 @@ from rich import print
 from rich.table import Table
 from rich.console import Console
 from datetime import date
+import json
 
-app: Typer = Typer()
+app: Typer = Typer()    
 
-class Todo:
-    def __init__(self, task: str, status: str) -> None:
-        self.id: int = 1
-        self.task: str = task
-        self.status: str = status
-        self.date_created: date = date.today()
+tasks: list[str] = []
 
-    def update_status(self, new_status: str):
-        self.status = new_status
+table: Table = Table(padding=(1))
 
-    def __repr__(self) -> str:
-        return f"{self.id}::{self.task}::{self.status}::{self.date_created}"
-    
+table.add_column("ID", justify="center", width=3)
+table.add_column("Task", width=30)
+table.add_column("Status", justify="center", no_wrap=True)
+table.add_column("Date Created", justify="center", no_wrap=True)
 
-tasks: list[Todo] = []
+console: Console = Console()
+
+def save_task(task: str) -> None:
+    data = load_tasks()
+    data.append(task)
+
+    with open("tasks.json", "w") as file:
+        json.dump(data, file)
+
+
+def load_tasks() -> list[str]:
+    try:
+        with open("tasks.json", "r") as file:
+            global tasks
+            tasks = json.load(file)
+    except FileNotFoundError:
+        with open("tasks.json", "w") as file:
+            json.dump([], file)
+
+    return tasks
 
 
 @app.command()
 def display() -> None:
-    table: Table = Table()
+    data: list[str] = load_tasks()
 
-    table.add_column("ID", justify="center", width=3)
-    table.add_column("Task", width=30)
-    table.add_column("Status", justify="center", no_wrap=True)
-    table.add_column("Date Created", justify="center", no_wrap=True)
+    for index, task in enumerate(data, 1):
+        info, status, time = task.split("::")
+        table.add_row(str(index), info, status, time)
 
-    for task in tasks:
-        id, info, status, time = str(task).split("::")
-        table.add_row(id, info, status, time)
-
-    console: Console = Console()
     console.print(table)
 
 
 @app.command()
-def add(task: str, status:str='incomplete') -> None:
-    status = status.lower()
-    if status not in ('complete', 'incomplete', 'in progress'):
-        print(f"\n[red]ERROR:[/red] '{status}' is not a valid status. "
-              "Defaulting to 'incomplete'.\n")
-        status = 'incomplete'
-
-    new_todo: Todo = Todo(task, status)
+def add(task: str) -> None:
+    new_todo: str = f"{task}::incomplete::{date.today()}"
     tasks.append(new_todo)
+    save_task(new_todo)
     display()
-
-@app.command()
-def options():
-    print("\ndisplay \n\n"
-          "[blue]add {task name} --status {status}[/blue]\n"
-          "add the given task name with a status to the list, if a status is"
-          "not given, it defaults to 'incomplete'.")
 
 
 if __name__ == "__main__":
